7737a3fcc12ddd07128deb3eb21c9cd07c33e860
xhiix <hiix_q1@protonmail.com>
2023-03-20 21:47:52 +0100

    mix phx.gen.auth

--- learnkz/mix.exs
+++ learnkz/mix.exs
@@ -32,6 +32,7 @@ defmodule Learnkz.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:pbkdf2_elixir, "~> 2.0"},
       {:phoenix, "~> 1.7.1"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
